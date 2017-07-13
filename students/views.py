16223259045492b02601f749a0384073ac5962c3
EmilSyvnii <emil.syvnii@gmail.com>
2017-07-13 14:07:47 +0300

    Edit students_list

--- students/views.py
+++ students/views.py
@@ -7,7 +7,29 @@ from django.http import HttpResponse
 # Views for Students.
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u'Даніель',
+         'last_name': u'Аггер',
+         'ticket': 5,
+         'image': 'img/Agger.jpg'},
+        {'id': 2,
+         'first_name': u'Садіо',
+         'last_name': u'Мане',
+         'ticket': 19,
+         'image': 'img/Sadio.jpeg'},
+        {'id': 3,
+         'first_name': u'Фернандо',
+         'last_name': u'Торрес',
+         'ticket': 9,
+         'image': 'img/Nando.jpg'},
+        {'id': 4,
+         'first_name': u'Джордан',
+         'last_name': u'Хендерсон',
+         'ticket': 14,
+         'image': 'img/Hendo.jpg'}
+    )
+    return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
