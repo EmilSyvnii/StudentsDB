2b1ef2a1a93108d64b2705fecc761bbfdac5d5f2
EmilSyvnii <emil.syvnii@gmail.com>
2017-08-03 20:13:51 +0300

    Separating views

--- students/views/students.py
+++ students/views/students.py
@@ -39,31 +39,3 @@ def students_edit(request, sid):
 
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete Student %s </h1>' % sid)
-
-def journal_list(request):
-    return HttpResponse('<h1>Journal</h1>')
-
-# Views for Groups
-
-def groups_list(request):
-    groups = (
-        {'id': 1,
-         'group_name': 'ЛФК-1',
-         'group_leader': u'Хендерсон Джордан'},
-        {'id': 2,
-         'group_name': 'ЛФК-2',
-         'group_leader': u'Аггер Даніель'},
-        {'id': 3,
-         'group_name': 'ЛФК-3',
-         'group_leader': u'Торрес Фернандо'}
-    )
-    return render(request, 'students/groups_list.html', {'groups': groups})
-
-def groups_add(request):
-    return HttpResponse('<h1>Group Add Form</h1>')
-
-def groups_edit(request, gid):
-    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
-
-def groups_delete(request, gid):
-    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
