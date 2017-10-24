a127ec4aa670e97b251cf27c6aa5a5741028f3c2
EmilSyvnii <emil.syvnii@gmail.com>
2017-10-24 17:46:23 +0300

    Adding model Student

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'students',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
