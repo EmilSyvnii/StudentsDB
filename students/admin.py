168364375885c86d1ac661c20c92382bca4787ae
EmilSyvnii <emil.syvnii@gmail.com>
2018-03-19 17:54:37 +0200

    Customizing admin's interface

--- students/admin.py
+++ students/admin.py
@@ -1,10 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.core.urlresolvers import reverse
 
 from django.contrib import admin
 from .models import Student, Group
+from django.forms import ModelForm, ValidationError
+
 # Register your models here.
 
-admin.site.register(Student)
+class StudentFormAdmin(ModelForm):
+    def clean_student_group(self):
+        groups = Group.objects.filter(leader=self.instance)
+        if len(groups) > 0 and self.cleaned_data['student_group'] != groups[0]:
+            raise ValidationError('Студент є старостою іншої групи', code='invalid')
+
+        return self.cleaned_data['student_group']
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'ticket', 'student_group']
+    list_display_links = ['last_name', 'first_name']
+    list_editable = ['student_group']
+    ordering = ['last_name']
+    list_filter = ['student_group']
+    list_per_page = 10
+    search_fields = ['last_name', 'first_name', 'ticket', 'student_group', 'notes']
+    form = StudentFormAdmin
+
+    def view_on_site(self, obj):
+        return reverse('students_edit', kwargs={'pk': obj.id})
+
+admin.site.register(Student, StudentAdmin)
 admin.site.register(Group)
 
